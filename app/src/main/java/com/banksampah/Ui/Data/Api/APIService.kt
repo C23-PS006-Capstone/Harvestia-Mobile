6585278f06e06976d88ffb12059cec92c203920e
beatrixstefany <beatrix.stefany@ti.ukdw.ac.id>
2023-06-15 21:44:09 +0700

    commit

---
+++ app/src/main/java/com/banksampah/Ui/Data/Api/APIService.kt
@@ -0,0 +1,16 @@
+package com.banksampah.Ui.Data.Api
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+class APIService {
+
+    @GET("/articles")
+    fun getArticles(
+        @Query("keyword") keyword: String,
+        @Query("judularticle") judularticle: String,
+    ): Call<ArticleResponse>
+
+}
