32daf177b222fd27b410e4c041b6fa9762656c3e
Rahmat Ramadhan <rakarr15@gmail.com>
2023-06-15 21:50:58 +0700

    buildconfig

---
+++ app/src/main/java/com/banksampah/Ui/UI/Preview/previewActivity.kt
@@ -0,0 +1,12 @@
+package com.banksampah.Ui.UI.Preview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.banksampah.R
+
+class previewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+    }
+}
