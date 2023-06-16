603771e77ad8eec2c4051f9862d9e9b62b3b6204
Timothy Ezra <122469086+tim-ezra@users.noreply.github.com>
2023-06-16 19:02:29 +0700

    Delete app directory

--- app/src/main/java/com/banksampah/Ui/Data/Api/History.kt
+++
@@ -1,7 +0,0 @@
-package com.banksampah.Ui.Data.Api
-
-import com.banksampah.Ui.Data.Respons.UserR
-
-interface History {
-    fun onDelete(modelDatabase: UserR?)
-}
--- app/src/main/java/com/banksampah/Ui/Data/Function/Format.kt
+++
@@ -1,8 +0,0 @@
-package com.banksampah.Ui.Data.Function
-
-import java.text.DecimalFormat
-
-fun rupiahFormat(price: Int): String {
-    val formatter = DecimalFormat("#,###")
-    return "Rp " + formatter.format(price.toLong())
-}
